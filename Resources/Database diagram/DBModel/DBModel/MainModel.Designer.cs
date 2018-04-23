9797cf193980a9832a1c62de20e4c4d8110651a7
Mali Imre Gergely <imimali18@gmail.com>
2018-04-23 23:16:10 +0300

    db model

--- Resources/Database diagram/DBModel/DBModel/MainModel.Designer.cs
+++ Resources/Database diagram/DBModel/DBModel/MainModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\David\source\repos\DBModel\DBModel\MainModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Gergely\Desktop\vivus\Resources\Database diagram\DBModel\DBModel\MainModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
