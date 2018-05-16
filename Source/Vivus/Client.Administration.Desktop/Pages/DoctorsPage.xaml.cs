c6dfde7fed27c77ca4830738f743c2e35de0eb80
Dumitrescu Dragos <dumitrescudragos03@gmail.com>
2018-05-16 20:36:36 +0300

    Add Administration -> DoctorsPage and Doctor -> PacientsPage Binding

--- Source/Vivus/Client.Administration.Desktop/Pages/DoctorsPage.xaml.cs
+++ Source/Vivus/Client.Administration.Desktop/Pages/DoctorsPage.xaml.cs
@@ -1,16 +1,59 @@
 ﻿namespace Vivus.Client.Administration.Desktop.Pages
 {
+    using System.Security;
+    using System.Windows.Controls;
+    using Vivus.Client.Core.AttachedProperties;
     using Vivus.Client.Core.Pages;
     using Vivus.Core.Administration.ViewModels;
+    using Vivus.Core.DataModels;
 
     /// <summary>
     /// Interaction logic for DoctorsPage.xaml
     /// </summary>
-    public partial class DoctorsPage : BasePage<DoctorsViewModel>
+    public partial class DoctorsPage : BasePage<DoctorsViewModel>, IPage, IContainPassword
     {
         public DoctorsPage()
         {
             InitializeComponent();
+
+            ViewModel.ParentPage = this;
+        }
+
+        /// <summary>
+        /// Stores a reference to the secured memory location of the password
+        /// </summary>
+        public SecureString SecurePasword => pbPassword.SecurePassword;
+
+        /// <summary>
+        /// Allows the errors to be displayed
+        /// </summary>
+        public void AllowErrors()
+        {
+            // Account details
+            tbEmail.SetValue(TextBoxExtensions.ShowErrorTemplateProperty, true);
+            pbPassword.GetBindingExpression(CacheModeProperty).UpdateTarget();
+            pbPassword.SetValue(PasswordBoxExtensions.ShowErrorTemplateProperty, true);
+
+            // Person details
+            tbFirstName.SetValue(TextBoxExtensions.ShowErrorTemplateProperty, true);
+            tbLastName.SetValue(TextBoxExtensions.ShowErrorTemplateProperty, true);
+            tbBirthDate.SetValue(TextBoxExtensions.ShowErrorTemplateProperty, true);
+            tbNin.SetValue(TextBoxExtensions.ShowErrorTemplateProperty, true);
+            tbPhoneNumber.SetValue(TextBoxExtensions.ShowErrorTemplateProperty, true);
+
+            // Home address
+            tbHomeStreetName.SetValue(TextBoxExtensions.ShowErrorTemplateProperty, true);
+            tbHomeStreetNo.SetValue(TextBoxExtensions.ShowErrorTemplateProperty, true);
+            tbHomeCity.SetValue(TextBoxExtensions.ShowErrorTemplateProperty, true);
+            cbHomeCounty.SetValue(ComboBoxExtensions.ShowErrorTemplateProperty, true);
+            tbHomeZipCode.SetValue(TextBoxExtensions.ShowErrorTemplateProperty, true);
+
+            // Work address
+            tbWorkStreetName.SetValue(TextBoxExtensions.ShowErrorTemplateProperty, true);
+            tbWorkStreetNo.SetValue(TextBoxExtensions.ShowErrorTemplateProperty, true);
+            tbWorkCity.SetValue(TextBoxExtensions.ShowErrorTemplateProperty, true);
+            cbWorkCounty.SetValue(ComboBoxExtensions.ShowErrorTemplateProperty, true);
+            tbWorkZipCode.SetValue(TextBoxExtensions.ShowErrorTemplateProperty, true);
         }
     }
 }
--- Source/Vivus/Core.Doctor/ViewModels/PatientsViewModel.cs
+++ Source/Vivus/Core.Doctor/ViewModels/PatientsViewModel.cs
@@ -1,6 +1,11 @@
 ﻿namespace Vivus.Core.Doctor.ViewModels
 {
+    using System.Collections.ObjectModel;
+    using System.Windows;
+    using System.Windows.Input;
+    using Vivus.Core.DataModels;
     using Vivus.Core.ViewModels;
+    using Vivus = Console;
 
     /// <summary>
     /// Represents a view model for the patients page.
@@ -8,5 +13,229 @@
 
     public class PatientsViewModel : BaseViewModel
     {
+        #region Public Properties
+
+        /// <summary>
+        /// Gets the collection of all the patients
+        /// </summary>
+        public ObservableCollection<PatientItemViewModel> AllPatients { get; }
+
+        /// <summary>
+        /// Gets the new patient command
+        /// </summary>
+        public ICommand NewPatientCommand { get; }
+
+        /// <summary>
+        /// Gets the choose command
+        /// </summary>
+        public ICommand ChooseCommand { get; }
+
+        /// <summary>
+        /// Gets the collection of own patients
+        /// </summary>
+        public ObservableCollection<PatientItemViewModel> MyPatients { get; }
+
+        /// <summary>
+        /// Get the dismiss command
+        /// </summary>
+        public ICommand DismissCommand { get; }
+
+        #endregion
+
+        #region Constructors
+
+        public PatientsViewModel()
+        {
+            AllPatients = new ObservableCollection<PatientItemViewModel>();
+            MyPatients = new ObservableCollection<PatientItemViewModel>();
+
+            NewPatientCommand = new RelayCommand(NewPatient);
+            ChooseCommand = new RelayCommand(ChoosePatient);
+            DismissCommand = new RelayCommand(DismissPatient);
+
+            // Test whether the binding was done right or not
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                AllPatients.Add(new PatientItemViewModel
+                {
+                    Id = 1,
+                    Name = "Patient One",
+                    BloodType = "AB4",
+                    Age = 18,
+                    Gender = "male",
+                    Status = "alive"
+                });
+
+                MyPatients.Add(new PatientItemViewModel
+                {
+                    Id = 2,
+                    Name = "Patient Two",
+                    BloodType = "A2",
+                    Age = 20,
+                    Gender = "female",
+                    Status = "dead"
+                });
+            });
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Add a new patient
+        /// </summary>
+        private void NewPatient()
+        {
+            Vivus.Console.WriteLine("Doctor: Add new patient!");
+            Popup("Successfull operation!", PopupType.Successful);
+        }
+
+        /// <summary>
+        /// Choose a patient
+        /// </summary>
+        private void ChoosePatient()
+        {
+            Vivus.Console.WriteLine("Doctor: Choose a patient!");
+            Popup("Successfull operation!", PopupType.Successful);
+        }
+
+        /// <summary>
+        /// Dismiss a patient
+        /// </summary>
+        private void DismissPatient()
+        {
+            Vivus.Console.WriteLine("Doctor: Dismiss a patient!");
+            Popup("Successfull operation!", PopupType.Successful);
+        }
+
+
+        #endregion
+    }
+
+    public class PatientItemViewModel : BaseViewModel
+    {
+        #region Private Members
+
+        private int id;
+        private string name;
+        private string bloodType;
+        private int age;
+        private string gender;
+        private string status;
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets or sets the id
+        /// </summary>
+        public int Id
+        {
+            get => id;
+
+            set
+            {
+                if (id == value)
+                    return;
+
+                id = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the name
+        /// </summary>
+        public string Name
+        {
+            get => name;
+
+            set
+            {
+                if (name == value)
+                    return;
+
+                name = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the blood type
+        /// </summary>
+        public string BloodType
+        {
+            get => bloodType;
+
+            set
+            {
+                if (bloodType == value)
+                    return;
+
+                bloodType = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the age
+        /// </summary>
+        public int Age
+        {
+            get => age;
+
+            set
+            {
+                if (age == value)
+                    return;
+
+                age = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the gender
+        /// </summary>
+        public string Gender
+        {
+            get => gender;
+
+            set
+            {
+                if (gender == value)
+                    return;
+
+                gender = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the status
+        /// </summary>
+        public string Status
+        {
+            get => status;
+
+            set
+            {
+                if (status == value)
+                    return;
+
+                status = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
     }
 }
