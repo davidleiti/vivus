a528b345d972af01afb150ebcabc3bea7eb8fe62
Leiti <leiti26@gmail.com>
2018-05-17 00:30:18 +0300

    Bound AdminstratorPage and DCPeronnel->NotificationsPage

--- Source/Vivus/Client.Administration.Desktop/Pages/AdministratorsPage.xaml.cs
+++ Source/Vivus/Client.Administration.Desktop/Pages/AdministratorsPage.xaml.cs
@@ -1,16 +1,42 @@
 ﻿namespace Vivus.Client.Administration.Desktop.Pages
 {
-    using Vivus.Client.Core.Pages;
+	using System.Security;
+	using Vivus.Client.Core.AttachedProperties;
+	using Vivus.Client.Core.Pages;
     using Vivus.Core.Administration.ViewModels;
+	using Vivus.Core.DataModels;
 
-    /// <summary>
-    /// Interaction logic for AdministratorsPage.xaml
-    /// </summary>
-    public partial class AdministratorsPage : BasePage<AdministratorsViewModel>
+	/// <summary>
+	/// Interaction logic for AdministratorsPage.xaml
+	/// </summary>
+	public partial class AdministratorsPage : BasePage<AdministratorsViewModel>, IPage, IContainPassword
     {
         public AdministratorsPage()
         {
             InitializeComponent();
-        }
-    }
+
+			ViewModel.ParentPage = this;
+		}
+
+		public SecureString SecurePasword => pbPassword.SecurePassword;
+
+		public void AllowErrors() {
+			tbEmail.SetValue(TextBoxExtensions.ShowErrorTemplateProperty, true);
+			//Gives an error, will have to look into it
+			//pbPassword.GetBindingExpression(CacheModeProperty).UpdateTarget();
+			pbPassword.SetValue(PasswordBoxExtensions.ShowErrorTemplateProperty, true);
+
+			tbFirstName.SetValue(TextBoxExtensions.ShowErrorTemplateProperty, true);
+			tbLastName.SetValue(TextBoxExtensions.ShowErrorTemplateProperty, true);
+			tbBirthDate.SetValue(TextBoxExtensions.ShowErrorTemplateProperty, true);
+			tbNin.SetValue(TextBoxExtensions.ShowErrorTemplateProperty, true);
+			tbPhoneNumber.SetValue(TextBoxExtensions.ShowErrorTemplateProperty, true);
+
+			tbStreetName.SetValue(TextBoxExtensions.ShowErrorTemplateProperty, true);
+			tbStreetNumber.SetValue(TextBoxExtensions.ShowErrorTemplateProperty, true);
+			tbCity.SetValue(TextBoxExtensions.ShowErrorTemplateProperty, true);
+			cbCounty.SetValue(ComboBoxExtensions.ShowErrorTemplateProperty, true);
+			tbZipCode.SetValue(TextBoxExtensions.ShowErrorTemplateProperty, true);
+		}
+	}
 }
