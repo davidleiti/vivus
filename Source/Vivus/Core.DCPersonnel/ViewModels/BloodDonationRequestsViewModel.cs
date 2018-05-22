423452729ec994bf4246a08c10c7cb4be68919d0
Mali Imre Gergely <imimali18@gmail.com>
2018-05-22 23:08:51 +0300

    added setters to properties

--- Source/Vivus/Core.DCPersonnel/ViewModels/BloodDonationRequestsViewModel.cs
+++ Source/Vivus/Core.DCPersonnel/ViewModels/BloodDonationRequestsViewModel.cs
@@ -28,46 +28,222 @@
         private string travelStatus;
         private string messages;
         private bool approved;
+        private ObservableCollection<BloodDonationRequestItem> bloodDonationRequestItems;
 
         #endregion
         #region Public Properties
         public IPage Parentpage { get; set; }
-        public string FullName { get => fullName; }
-        public string Nin { get => nin; }
-        public int? Age { get => age; }
-        public int? Weight { get => weight; }
-        public int? HeartRate { get => heartRate; }
-        public int? SystolicBP { get => systolicBP; }
-        public int? DiastolicBP { get => diastolicBP; }
-        public DateTime? ApplyDate { get => applyDate; }
-        public string PastSurgeries { get => pastSurgeries; }
-        public string TravelStatus { get => travelStatus; }
+        public string FullName
+        {
+            get => fullName;
+            set
+            {
+                if (fullName == value)
+
+                    return;
+
+                fullName = value;
+
+                OnPropertyChanged();
+
+            }
+
+        }
+        public string Nin
+        {
+            get => nin;
+            set
+            {
+                if (nin == value)
+
+                    return;
+
+                nin = value;
+
+                OnPropertyChanged();
+
+            }
+        }
+        public int? Age
+        {
+            get => age;
+            set
+            {
+                if (age == value)
+
+                    return;
+
+                age = value;
+
+                OnPropertyChanged();
+
+            }
+
+        }
+        public int? Weight
+        {
+            get => weight;
+            set
+            {
+                if (weight == value)
+
+                    return;
+
+                weight = value;
+
+                OnPropertyChanged();
+            }
+
+        }
+        public int? HeartRate
+        {
+            get => heartRate;
+
+            set
+            {
+                if (heartRate == value)
+
+                    return;
+
+                heartRate = value;
+
+                OnPropertyChanged();
+
+
+            }
+        }
+        public int? SystolicBP
+        {
+            get => systolicBP;
+
+            set
+            {
+                if (systolicBP == value)
+
+                    return;
+
+                systolicBP = value;
+
+                OnPropertyChanged();
+            }
+        }
+        public int? DiastolicBP
+        {
+            get => diastolicBP;
+
+            set
+            {
+                if (diastolicBP == value)
+
+                    return;
+
+                diastolicBP = value;
+
+                OnPropertyChanged();
+            }
+        }
+        public DateTime? ApplyDate
+        {
+            get => applyDate;
+
+            set
+            {
+                if (applyDate == value)
+
+                    return;
+
+                applyDate = value;
+
+                OnPropertyChanged();
+
+            }
+
+        }
+
+        public string PastSurgeries
+        {
+
+            get => pastSurgeries;
+
+            set
+            {
+                if (pastSurgeries == value)
+
+                    return;
+
+                pastSurgeries = value;
+
+                OnPropertyChanged();
+            }
+        }
+        public string TravelStatus
+        {
+            get => travelStatus;
+
+            set
+            {
+                if (travelStatus == value)
+
+                    return;
+
+                travelStatus = value;
+
+                OnPropertyChanged();
+
+            }
+
+        }
         public string Messages
         {
             get => messages;
             set
             {
-                if (messages == value) return;
+                if (messages == value)
+
+                    return;
+
                 messages = value;
+
                 OnPropertyChanged();
             }
         }
         public bool Approved
         {
             get => approved;
+
             set
             {
-                if (approved == value) return;
+                if (approved == value)
+
+                    return;
+
                 approved = value;
+
                 OnPropertyChanged();
             }
         }
-        public ObservableCollection<BloodDonationRequestItem> BloodDonationRequestItems { get; set; }
+        public ObservableCollection<BloodDonationRequestItem> BloodDonationRequestItems
+        {
+            get => bloodDonationRequestItems;
+            set
+            {
+                if (bloodDonationRequestItems == value)
+
+                    return;
+
+                bloodDonationRequestItems = value;
+
+                OnPropertyChanged();
+
+            }
+
+        }
         public override string this[string propertyName]
         {
             get
             {
-                if (propertyName == nameof(Messages)) return GetErrorString(propertyName, DCPersonnelValidator.MessagesValidation(Messages));
+                if (propertyName == nameof(Messages))
+                    return GetErrorString(propertyName, DCPersonnelValidator.MessagesValidation(Messages));
                 return null;
             }
         }
@@ -125,21 +301,26 @@
         private string fullName;
         private int age;
         #endregion
+
+        #region Public Properties
         public int Id
         {
             get => id;
             set
             {
                 if (value == id)
+
                     return;
+
                 id = value;
+
                 OnPropertyChanged();
             }
         }
-        #region Public Properties
         public string FullName
         {
             get => fullName;
+
             set
             {
                 if (fullName == value) return;
@@ -150,30 +331,42 @@
         public string Nin
         {
             get => nin;
+
             set
             {
-                if (nin == value) return;
+                if (nin == value)
+                    return;
+
                 nin = value;
+
                 OnPropertyChanged();
             }
         }
         public int Age
         {
             get => age;
+
             set
             {
-                if (age == value) return;
+                if (age == value)
+                    return;
+
                 age = value;
+
                 OnPropertyChanged();
             }
         }
         public DateTime ApplyDate
         {
             get => applyDate;
+
             set
             {
-                if (applyDate == value) return;
+                if (applyDate == value)
+                    return;
+
                 applyDate = value;
+
                 OnPropertyChanged();
             }
         }
