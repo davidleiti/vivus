0f8d6463c901f70cb8d140b15e069bb7f1802a7f
David Perisanu <david.perisanu@gmail.com>
2018-05-23 12:46:13 +0300

    Modify unit test projects.

--- Test/Vivus.Core.DCPersonnel.UnitTests/Properties/AssemblyInfo.cs
+++ Test/Vivus.Core.DCPersonnel.UnitTests/Properties/AssemblyInfo.cs
@@ -2,19 +2,19 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Vivus.Core.Doctor.UnitTests")]
+[assembly: AssemblyTitle("Vivus Core DCPersonnel UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Vivus.Core.Doctor.UnitTests")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("Vivus Core DCPersonnel UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Vivus 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("6212d4a9-f8bf-45d6-b049-35dfa4921d03")]
+[assembly: Guid("84dac11c-7754-4448-bb99-6761b3241bde")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
--- Test/Vivus.Core.UnitTests/Properties/AssemblyInfo.cs
+++ Test/Vivus.Core.UnitTests/Properties/AssemblyInfo.cs
@@ -2,19 +2,19 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Vivus.Core.Donor.UnitTests")]
+[assembly: AssemblyTitle("Vivus Core UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Vivus.Core.Donor.UnitTests")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("Vivus Core UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Vivus 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("1bda6d60-fb50-4a6a-8032-fe2b7f79e669")]
+[assembly: Guid("c427de44-82be-4376-bcdf-ba7d001ef9cc")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
