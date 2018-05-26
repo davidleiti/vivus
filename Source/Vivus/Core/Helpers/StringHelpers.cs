dce76738e6cc4bae4617c016c2306fedcfab90f9
David Perisanu <david.perisanu@gmail.com>
2018-05-26 20:31:57 +0300

    Add StringHelpers.

---
+++ Source/Vivus/Core/Helpers/StringHelpers.cs
@@ -0,0 +1,23 @@
+﻿namespace Vivus.Core.Helpers
+{
+    /// <summary>
+    /// Represents a collection of methods to facilitate the work with string.
+    /// </summary>
+    public static class StringHelpers
+    {
+        /// <summary>
+        /// Checks whether the current string contains one of the specified patterns.
+        /// </summary>
+        /// <param name="str">The current string.</param>
+        /// <param name="patterns">The collection of patterns.</param>
+        /// <returns></returns>
+        public static bool Contains(this string str, string[] patterns)
+        {
+            foreach (string pattern in patterns)
+                if (str.ToLower().Contains(pattern.ToLower()))
+                    return true;
+
+            return false;
+        }
+    }
+}
