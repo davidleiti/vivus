234cb82fe9ce11f8b08022c7234d9548d689b384
Luca Alex <alex_luca29@yahoo.com>
2018-05-26 19:23:58 +0300

    have some problems with the request

--- Source/Vivus/Core.Doctor/ViewModels/RequestViewModel.cs
+++ Source/Vivus/Core.Doctor/ViewModels/RequestViewModel.cs
@@ -9,6 +9,12 @@
     using System.Windows.Input;
     using Vivus = Console;
     using Vivus.Core.Doctor.Validators;
+    using Vivus.Core.UoW;
+    using Vivus.Core.ViewModels.Base;
+    using Vivus.Core.Security;
+    using Vivus.Core.Doctor.IoC;
+    using System.Threading.Tasks;
+    using System.Linq;
 
     /// <summary>
     /// Represents a view model for the request page.
@@ -24,6 +30,11 @@
         private int? requestRedCells;
         private int? requestPlasma;
         private int? requestBlood;
+        private IUnitOfWork unitOfWork;
+        private IApllicationViewModel<Model.Doctor> appViewModel;
+        private ISecurity security;
+        private RequestItemViewModel selectedItem;
+
 
         #endregion
 
@@ -160,6 +171,25 @@
             }
         }
 
+        /// <summary>
+		/// Gets or sets the selected item in the administrators table
+		/// </summary>
+		public RequestItemViewModel SelectedItem
+        {
+            get => selectedItem;
+
+            set
+            {
+                if (value == selectedItem)
+                    return;
+
+                selectedItem = value;
+                
+                OnPropertyChanged();
+            }
+        }
+
+
         public override string this[string propertyName]
         {
             get
@@ -193,19 +223,69 @@
         /// <summary>
         /// Initializes a new instance of the <see cref="RequestViewModel"/> class with the default values.
         /// </summary>
-        public RequestViewModel()
+        public RequestViewModel() : base(new DispatcherWrapper(Application.Current.Dispatcher))
         {
 
             Items = new ObservableCollection<RequestItemViewModel>();
             PatientNames = new List<BasicEntity<string>> { new BasicEntity<string>(-1, "Select patient") };
             Priorities = new List<BasicEntity<string>> { new BasicEntity<string>(-1, "Select priority") };
-            AddCommand = new RelayCommand(Add);
+            AddCommand = new RelayCommand(async () => await AddAsync());
             CancelCommand = new RelayCommand(Cancel);
 
+            unitOfWork = IoCContainer.Get<IUnitOfWork>();
+            appViewModel = IoCContainer.Get<IApllicationViewModel<Model.Doctor>>();
+            security = IoCContainer.Get<ISecurity>();
 
-
+            Task.Run(async () =>
+            {
+                await LoadPatientsAsync();
+                await LoadPrioritiesAsync();
+                PopulateFields();
+            });
 
             // Test whether the binding was done right or not
+            /*
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                Items.Add(new RequestItemViewModel
+                {
+                    Id = 39,
+                    PatientName = "Ionut",
+                    Priority = "easy",
+                    Thrombocytes = 1,
+                    RedCells = 213,
+                    Plasma = 555,
+                    RequestStatus = "done",
+                    Cdc = "???",
+                    Dcn = "dcn"
+                });
+
+            });*/
+        }
+
+
+        public RequestViewModel(IUnitOfWork unitOfWork, IApllicationViewModel<Model.Doctor> appViewModel, IDispatcherWrapper dispatcherWrapper, ISecurity security) : base(new DispatcherWrapper(Application.Current.Dispatcher))
+        {
+
+            Items = new ObservableCollection<RequestItemViewModel>();
+            PatientNames = new List<BasicEntity<string>> { new BasicEntity<string>(-1, "Select patient") };
+            Priorities = new List<BasicEntity<string>> { new BasicEntity<string>(-1, "Select priority") };
+            AddCommand = new RelayCommand(async () => await AddAsync());
+            CancelCommand = new RelayCommand(Cancel);
+
+            this.unitOfWork = unitOfWork;
+            this.appViewModel = appViewModel;
+            this.security = security;
+
+            Task.Run(async () =>
+            {
+                await LoadPatientsAsync();
+                await LoadPrioritiesAsync();
+                PopulateFields();
+            });
+
+            // Test whether the binding was done right or not
+            
             Application.Current.Dispatcher.Invoke(() =>
             {
                 Items.Add(new RequestItemViewModel
@@ -223,15 +303,166 @@
 
             });
         }
+
         #endregion
 
         #region Private Methods
 
         /// <summary>
+        /// Loads all the patients asynchronously.
+        /// </summary>
+        /// <returns></returns>
+        private async Task LoadPatientsAsync()
+        {
+            await Task.Run(() =>
+            {
+                PatientNames.Clear();
+                PatientNames.Add(new BasicEntity<string>(-1, "Select patient"));
+                unitOfWork.Patients
+                            .Entities
+                            .ToList()
+                            .ForEach(patient => dispatcherWrapper
+                            .InvokeAsync(() => PatientNames.Add(new BasicEntity<string>(patient.PersonID,
+                                                                 patient.Person.FirstName + " " + patient.Person.LastName)))
+                );
+            });
+        }
+
+        /// <summary>
+        /// Loads all the patients asynchronously.
+        /// </summary>
+        /// <returns></returns>
+        private async Task LoadPrioritiesAsync()
+        {
+            await Task.Run(() =>
+            {
+                Priorities.Clear();
+                Priorities.Add(new BasicEntity<string>(-1, "Select priority"));
+                unitOfWork.RequestPriorities.Entities.ToList().ForEach(priority =>
+                    dispatcherWrapper.InvokeAsync(() => Priorities.Add(new BasicEntity<string>(priority.RequestPriorityID, priority.Type)))
+                );
+            });
+        }
+
+        /// <summary>
+        /// Clears all the fields of the viewmodel.
+        /// </summary>
+        private void ClearFields()
+        {
+            SelectedPatientName = new BasicEntity<string>(-1, "Select patient");
+            SelectedPriority = new BasicEntity<string>(-1, "Select priority");
+            RequestThrombocytes = 0;
+            RequestRedCells = 0;
+            RequestPlasma = 0;
+            RequestBlood = 0;
+        }
+
+        /// <summary>
+        /// Populates all the fields of the viewmodel.
+        /// </summary>
+        private void PopulateFields()
+        {               
+            SelectedPatientName = new BasicEntity<string>(selectedItem.Id, selectedItem.PatientName);
+            SelectedPriority = new BasicEntity<string>(selectedItem.Id, selectedItem.Priority);
+            RequestThrombocytes = selectedItem.Thrombocytes;
+            RequestRedCells = selectedItem.RedCells;
+            //RequestBlood = selectedItem.Blood;
+        }
+
+        /// <summary>
+        /// Fills the fields of a request.
+        /// </summary>
+        /// <param name="request">The request instance.</param>
+        /// <param name="fillOptional">Whether to fill the optional field also or not.</param>
+        private void FillModelRequest(ref Model.BloodRequest request, bool fillOptional = false)
+        {
+            Model.Patient patient;
+            patient = unitOfWork.Patients.Find(p => p.Person.PersonID == SelectedPatientName.Id).Single();
+            Model.RequestPriority priority;
+            priority = unitOfWork.RequestPriorities.Find(r => r.RequestPriorityID == SelectedPriority.Id).Single();
+
+            // If the instance is null, initialize it
+            if (request is null)
+                request = new Model.BloodRequest();
+
+
+            // Update the database
+
+            //request.Patient = patient;
+            //request.RequestPriority = priority;
+            //request.ThrombocytesQuantity = RequestThrombocytes;
+            //request.RedCellsQuantity = RequestRedCells;
+            //request.PlasmaQuantity = RequestPlasma;
+            //request.BloodQuantity = RequestBlood;
+        }
+
+        /// <summary>
+        /// Fills the fields of a request viewmodel.
+        /// </summary>
+        /// <param name="request">The request viewmodel instance.</param>
+        private void FillViewModelRequest(ref RequestItemViewModel request)
+        {
+            Model.Patient patient;
+            patient = unitOfWork.Patients.Find(p => p.Person.PersonID == SelectedPatientName.Id).Single();
+            Model.RequestPriority priority;
+            priority = unitOfWork.RequestPriorities.Find(r => r.RequestPriorityID == SelectedPriority.Id).Single();
+
+            // If the instance is null, initialize it
+            if (request is null)
+                request = new RequestItemViewModel();
+
+            request.PatientName = patient.Person.FirstName +" "+ patient.Person.LastName;
+            request.Priority = priority.ToString();
+            request.Thrombocytes = RequestThrombocytes;
+            request.RedCells = RequestRedCells;
+            request.Plasma = RequestPlasma;
+            request.Blood = RequestBlood;
+        }
+
+
+        /// <summary>
         /// Add.
         /// </summary>
-        private void Add()
+        private async Task AddAsync()
         {
+
+            await Task.Run(() =>
+            {
+                dispatcherWrapper.InvokeAsync(() => ParentPage.AllowErrors());
+
+                if (Errors > 0)
+                {
+                    Popup("Some errors were found. Fix them before going forward.");
+                    return;
+                }
+
+                try
+                {
+                    Model.BloodRequest request;
+                    RequestItemViewModel requestViewModel;
+
+                    request = null;
+                    requestViewModel = null;
+
+                    FillModelRequest(ref request, true);
+                    unitOfWork.BloodRequests.Add(request);
+                    // Make changes persistent
+                    unitOfWork.Complete();
+
+                    // Update the table
+                    FillViewModelRequest(ref requestViewModel);
+                    requestViewModel.Id = request.PatientID;
+
+                    dispatcherWrapper.InvokeAsync(() => Items.Add(requestViewModel));
+
+                    Popup($"Request added successfully!", PopupType.Successful);
+                }
+                catch
+                {
+                    Popup($"An error occured while adding the request.");
+                }
+            });
+            /*
             ParentPage.AllowErrors();
 
             if(Errors > 0)
@@ -241,7 +472,7 @@
             }
 
             Vivus.Console.WriteLine("Doctor: Add works!");
-            Popup("Add works!", PopupType.Successful);
+            Popup("Add works!", PopupType.Successful);*/
         }
 
         /// <summary>
@@ -275,9 +506,10 @@
         private int id;
         private string patientName;
         private string priority;
-        private int thrombocytes;
-        private int redCells;
-        private int plasma;
+        private int? thrombocytes;
+        private int? redCells;
+        private int? plasma;
+        private int? blood;
         private string requestStatus;
         private string cdc;
         private string dcn;
@@ -343,7 +575,7 @@
         /// <summary>
         /// Gets or sets the thrombocytes.
         /// </summary>
-        public int Thrombocytes
+        public int? Thrombocytes
         {
             get => thrombocytes;
 
@@ -361,7 +593,7 @@
         /// <summary>
         /// Gets or sets the red cells.
         /// </summary>
-        public int RedCells
+        public int? RedCells
         {
             get => redCells;
 
@@ -379,7 +611,7 @@
         /// <summary>
         /// Gets or sets the plasma.
         /// </summary>
-        public int Plasma
+        public int? Plasma
         {
             get => plasma;
 
@@ -389,6 +621,25 @@
                     return;
 
                 plasma = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+
+        /// <summary>
+        /// Gets or sets the blood.
+        /// </summary>
+        public int? Blood
+        {
+            get => blood;
+
+            set
+            {
+                if (blood == value)
+                    return;
+
+                blood = value;
 
                 OnPropertyChanged();
             }
--- Source/Vivus/Core.Donor/ViewModels/ProfileViewModel.cs
+++ Source/Vivus/Core.Donor/ViewModels/ProfileViewModel.cs
@@ -154,7 +154,7 @@
                 if (propertyName == nameof(Password) && ParentPage != null)
                     return GetNotMandatoryErrorString(propertyName, DonorValidator.PasswordValidation((ParentPage as IContainPassword).SecurePasword));
 
-                if (propertyName == nameof(SelectedDonationCenter))
+                if (propertyName == nameof(SelectedDonationCenter) && SelectedDonationCenter != null)
                     return GetErrorString(propertyName, DonorValidator.FavouriteDonationCenterValidation(SelectedDonationCenter));
 
                 return null;
