e6fb73b4a2dea13c69d6b4545d7d9c9f256c4f04
Luca Alex <alex_luca29@yahoo.com>
2018-05-26 10:54:44 +0300

    started to do some tests

---
+++ Test/Vivus.Core.Donor.UnitTests/ViewModels/ProfileViewModelTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Moq;
+using Vivus.Core.DataModels;
+using Vivus.Core.Donor.ViewModels;
+using Vivus.Core.UnitTests.Dependencies;
+using Vivus.Core.UnitTests.Dependencies.UoW;
+using Vivus.Core.ViewModels.Base;
+
+namespace Vivus.Core.Donor.UnitTests.ViewModels
+{
+    class ProfileViewModelTests
+    {
+        private UnitOfWork unitOfWork;
+        private Mock<IApllicationViewModel<Model.Donor>> appViewModel;
+        private Core.DataModels.DispatcherWrapper dispatcherWrapper;
+       // private Security security;
+        private ParentPage parentPage;
+        private ProfileViewModel viewModel;
+    }
+}
