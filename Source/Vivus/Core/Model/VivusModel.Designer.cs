77cd8c8aff5edd18e0ba0f829f341f8fb38407e7
David Perisanu <david.perisanu@gmail.com>
2018-05-27 16:10:08 +0300

    Add BloodTypeID and RhID to Donor class.

--- Source/Vivus/Core/Model/VivusModel.Designer.cs
+++ Source/Vivus/Core/Model/VivusModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Xvid1\Documents\GitHub\vivus\Source\Vivus\Core\Model\VivusModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Git Repostories\vivus\Source\Vivus\Core\Model\VivusModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
